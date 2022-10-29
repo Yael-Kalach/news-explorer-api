c5064829fed706caec78304fe5abfe5d8da431aa
Yael-Kalach <ylklk13579@gmail.com>
2022-10-29 11:49:46 +0300

    debugging

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
-// const { celebrate } = require('celebrate');
+const { celebrate } = require('celebrate');
 const {
   getUsers,
   getUserById,
 } = require('../controllers/users');
-const {auth} = require('../middlewares/auth')
-// const {
-//   getCurrentUserSchema,
-// } = require('../utils/validators');
+const {
+  getCurrentUserSchema,
+} = require('../utils/validators');
 
 router.get('/', getUsers);
-router.get('/me', auth, getUserById);
-// router.get('/:userId', celebrate(getCurrentUserSchema), getUserById);
+router.get('/me', getUserById);
+router.get('/:userId', celebrate(getCurrentUserSchema), getUserById);
 
 module.exports = router;
